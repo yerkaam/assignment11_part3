cbe1382c5b6dc149976c6e7c1993e8f7c3fdf468
yerkaam <eraorynbai@gmail.com>
2023-12-03 16:10:58 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+     <div>
+        <h1>Hello world</h1>
+        <div class="flex" style="margin-left: -190px;">
+            <p>Type your name:</p>
+        <input @input="addText" type="text" id="input">
+        <button @click="deleteText">Delete</button>
+        </div>
+        <div  class="">
+        <p id="text">Your name:</p>
+        </div>
+     </div>
+</template>
+<script>
+
+export default{
+    data(){
+        return{
+            text:'Your name:'
+        }
+    },
+    methods:{
+        addText(){
+            let te = this.text;
+            te += document.getElementById('input').value.trim()
+            let t = document.getElementById('text')
+            t.textContent = te;
+        },
+        deleteText(){
+         document.getElementById('input').value = '';
+ document.getElementById('text').textContent =''
+            
+    }
+    }
+    
+}
+</script>
+<style>
+body{
+    background-color: aqua;
+}
+#app{
+    width: 500px;
+    margin: 0 auto;
+}
+.flex{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+input{
+    background-color: transparent;
+    border: 1px solid black;
+    height: fit-content;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { createApp } from 'vue'
+import App from '/Users/eraorynbaigmail.com/Desktop/assignment/part3/src/App.vue'
+createApp(App).mount('#app')
